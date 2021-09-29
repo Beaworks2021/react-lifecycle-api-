653ad4e224b34fccdb26e6d3b8c14b4a28e74ad9
Akaditi <you@example.com>
2021-09-29 12:02:43 +0000

    first commit

---
+++ src/Components/UsersClass.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export class UsersClass extends React.Component{
+    constructor() {
+        super();
+        this.state = {users: []};
+      }
+    
+
+      async getUsers() {
+        
+        fetch("https://jsonplaceholder.typicode.com/users")
+          .then((response) => {        
+            return response.json();
+          })
+          .then((users) => {
+            this.setState({ users: users });
+          });
+      }
+
+      componentDidMount() {
+        this.getUsers();
+      }
+    
+    
+      render() {
+        return   <>
+        {this.state.users.map((user) => {
+          return <h1 key={user.id}>{user.name}<br/>{user.username}<br/>{user.email}<br/><br/></h1>;
+        })}
+      </>
+      }
+    }
+
+    export default UsersClass
